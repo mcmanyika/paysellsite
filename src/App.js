299d048b7adba425eae24cd616676fdd1765816d
mcmanyika <mcmanyika.com>
2023-05-14 16:23:45 -0500

    initial push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Web3Button } from '@web3modal/react'
+
+function App() {
+  return (
+    <div className=' w-96 mx-auto'>
+      <Web3Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { arbitrum, mainnet, polygon } from 'wagmi/chains'
+
+const chains = [arbitrum, mainnet, polygon]
+const projectId = '600d51f1ffdb7a0e0e8ac2e2acbc3b75'
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
+  publicClient
+})
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <>
+      <WagmiConfig config={wagmiConfig}>
+        <App />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  </React.StrictMode>
+);
