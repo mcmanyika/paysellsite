a52587f6f93045f6ade820af4ecf46bece0b1cac
mcmanyika <mcmanyika.com>
2023-05-14 16:54:40 -0500

    added banner

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import { Web3Button } from '@web3modal/react'
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center w-5xl mx-auto'>
+    <div className='flex flex-col items-center justify-center w-5xl mx-auto p-3'>
       <Web3Button />
+      <div className="p-3">
+          <img src="/3.jpeg" alt="" width={600} className='rounded-tl-lg rounded-tr-lg' />
+        </div>
     </div>
   );
 }
